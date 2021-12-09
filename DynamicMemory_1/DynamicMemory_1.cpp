f5cdd0e81a276338da81c76b18273443cdb28a54
trimmergrom@yandex.ru <trimmergrom@yandex.ru>
2021-12-09 16:15:53 +0300

    Dynamic_Memory_master

--- DynamicMemory_1/DynamicMemory_1.cpp
+++ DynamicMemory_1/DynamicMemory_1.cpp
@@ -15,7 +15,13 @@ int* Insert(int* arr,  int &arr_length,  int* namber, int* element);
 int* Pop_back(int* arr, int &arr_length);
 int* Pop_front(int* arr, int &arr_length);
 int* Erase(int* arr, int& arr_length, int* namber);
-void Delete(int** arr, const int rows, const int cols);
+int** Push_row_back(int** arr, int& rows, const int cols);
+int** Push_row_front(int** arr, int& rows, const int cols);
+int** Insert_row(int** arr, int& rows, const int cols, int& namber);
+void Push_col_back(int** arr, const int rows, int& cols);
+void Push_col_front(int** arr, const int rows, int& cols);
+void Erase_col(int** arr, const int rows, int& cols, int& namber);
+void clear(int** arr, const int rows);
 
 int main()
 {
@@ -77,13 +83,44 @@ int main()
 #ifdef DYNAMIC_MEMORY_2
 	int ROWS;
 	int COLS;
+	int namber;
 	std::cout << "Enter ROWS "; std::cin >> ROWS; std::cout << std::endl;
 	std::cout << "Enter COLS "; std::cin >> COLS; std::cout << std::endl;
 	int** arr = allocate(ROWS, COLS);
 	FillRand(arr, ROWS, COLS);
 	Print(arr, ROWS, COLS);
+	std::cout << "\n========================================================\n";
+	std::cout << "Push_row_back \n";
+	arr = Push_row_back(arr, ROWS, COLS);
+	Print(arr, ROWS, COLS);
+	std::cout << "\n========================================================\n";
+	
+	std::cout << "Push_row_front \n";
+	arr = Push_row_front(arr, ROWS, COLS);
+	Print(arr, ROWS, COLS);
+	
+	std::cout << "\n========================================================\n";
+	std::cout << "Enter the insertion position number "; std::cin >> namber; std::cout << "\n";
+	arr = Insert_row(arr, ROWS, COLS, namber);
+	Print(arr, ROWS, COLS);
 
-	Delete(arr, ROWS, COLS);
+	std::cout << "\n========================================================\n";
+	std::cout << "Push_col_back \n";
+	Push_col_back(arr, ROWS, COLS);
+	Print(arr, ROWS, COLS);
+
+	std::cout << "\n========================================================\n";
+	std::cout << "Push_col_front \n";
+	Push_col_front(arr, ROWS, COLS);
+	Print(arr, ROWS, COLS);
+
+	std::cout << "\n========================================================\n";
+	std::cout << "Erase_col \n";
+	std::cout << "Enter the number of the position to be deleted "; std::cin >> namber; std::cout << "\n";
+	Erase_col(arr, ROWS, COLS, namber);
+	Print(arr, ROWS, COLS);
+
+	clear(arr, ROWS);
 
 #endif // DYNAMIC_MEMORY_2
 
@@ -181,7 +218,69 @@ int* Erase(int* arr, int &arr_length, int* namber)
 	delete[] arr;
 	return buffer;	
 }
-void Delete(int** arr, const int rows, const int cols)
+int** Push_row_back(int** arr,  int& rows, const int cols)
+{
+	int** buffer = new int* [rows+1]{};
+	for (int i = 0; i < rows; i++) buffer[i] = arr[i];
+	delete[] arr;
+	buffer[rows] = new int[cols] {};
+	rows++;
+	return buffer;
+}
+int** Push_row_front(int** arr, int& rows, const int cols)
+{
+	int** buffer = new int* [rows + 1]{};
+	for (int i = 0; i < rows; i++) buffer[i+1] = arr[i];
+	delete[] arr;
+	buffer[0] = new int[cols] {};
+	rows++;
+	return buffer;
+}
+int** Insert_row(int** arr, int& rows, const int cols, int& namber)
+{
+	int** buffer = new int* [rows + 1]{};
+	for (int i = 0; i < namber; i++) buffer[i] = arr[i];
+	for (int i = namber + 1; i <= rows; i++) buffer[i] = arr[i-1];
+	delete[] arr;
+	buffer[namber] = new int[cols] {};
+	rows++;
+	return buffer;
+}
+void Push_col_back(int** arr, const int rows, int& cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols + 1]{};
+		for (int j = 0; j < cols; j++) buffer[j] = arr[i][j];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols++;	
+}
+void Push_col_front(int** arr, const int rows, int& cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols + 1]{};
+		for (int j = 1; j < cols; j++) buffer[j] = arr[i][j];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols++;
+}
+void Erase_col(int** arr, const int rows, int& cols, int& namber)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols - 1]{};
+		for (int j = 0; j < namber -1; j++) buffer[j] = arr[i][j];
+		for (int j = namber - 1; j < cols; j++) buffer[j] = arr[i][j + 1];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols--;
+}
+void clear(int** arr, const int rows)
 {
 	for (int i = 0; i < rows; i++)
 	{
